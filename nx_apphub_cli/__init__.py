e329b4b6c6e00b8146f99aadb3f73cd5012ecd8b
Uri Herrera <hello@uriherrera.com>
2026-05-19 15:50:05 -0600

    bump version for new release

--- nx_apphub_cli/__init__.py
+++ nx_apphub_cli/__init__.py
@@ -26,4 +26,4 @@ __all__ = [
     "search",
 ]
 
-__version__ = "1.1.1"
+__version__ = "1.2.1"
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 # --->
 setup(
     name="nx-apphub-cli",
-    version="1.1.1",
+    version="1.2.1",
     packages=find_packages(),
     install_requires=[
         "requests",
